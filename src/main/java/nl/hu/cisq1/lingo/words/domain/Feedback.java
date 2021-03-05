1f5093e3fa19f82853581c79dcefff360678384f
Etienne Hosman <etienne.hosman@student.hu.nl>
2021-03-05 16:55:30 +0100

    tests and hint

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Feedback.java
@@ -2,17 +2,20 @@ package nl.hu.cisq1.lingo.words.domain;
 
 import nl.hu.cisq1.lingo.words.domain.Mark;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Feedback {
     private String attempt;
     private List<Mark> marks;
+    private String hint;
 
     public Feedback(String attempt, List<Mark> marks)
     {
         this.attempt = attempt;
         this.marks = marks;
+        this.hint = "";
     }
 
     public boolean isWordGuessed() {
@@ -23,6 +26,22 @@ public class Feedback {
     public boolean isWordValid() {
         return  marks.stream()
                 .noneMatch( mark -> mark == Mark.INVALID);
+    }
+
+    public String giveHint(String previousHint, String wordToGuess){
+        for (int i = 0; i < this.marks.size(); i++){
+            if(this.marks.get(i).equals(Mark.CORRECT) || previousHint.charAt(i) != '.' && previousHint.charAt(i) != '*'){
+                this.hint += wordToGuess.charAt(i);
+            } else if(this.marks.get(i).equals(Mark.PRESENT) ){
+                this.hint += '*';
+            }
+            else if (this.marks.get(i).equals(Mark.ABSENT)){
+                this.hint += '.';
+            }else {
+                this.hint += '.';
+            }
+        }
+        return this.hint;
     }
 
     @Override
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -4,12 +4,25 @@ import nl.hu.cisq1.lingo.words.domain.Feedback;
 import nl.hu.cisq1.lingo.words.domain.Mark;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FeedbackTest {
+
+    static Stream<Arguments> provideHintExamples(){
+        return Stream.of(
+            Arguments.of("w....", "words", "word", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID), "w...."),
+            Arguments.of("w....", "words", "water", List.of(Mark.CORRECT,Mark.ABSENT,Mark.ABSENT,Mark.ABSENT, Mark.PRESENT), "w...*"),
+            Arguments.of("w...*", "words", "worst", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.PRESENT, Mark.ABSENT), "wor*."),
+            Arguments.of("wor*.", "words", "words", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT), "words")
+        );
+    }
     @Test
     @DisplayName("word is guessed if all letters are correct")
     void wordIsGuessed() {
@@ -36,4 +49,13 @@ class FeedbackTest {
         Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
         assertTrue(feedback.isWordValid());
     }
+
+    @ParameterizedTest
+    @MethodSource("provideHintExamples")
+    @DisplayName("guess gives correct hint")
+    void guessHasHint(String previousHint, String wordToGuess, String guess, List<Mark> marks, String hint){
+        Feedback feedback = new Feedback(guess, marks);
+        assertEquals(hint, feedback.giveHint(previousHint, wordToGuess));
+    }
+
 }
